fa5561e1ce290839163ab86288b8a18f63d56bff
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-09-17 00:23:12 +0700

    fixing errors and change version

---
+++ Incremental-Game/Assets/Scripts/ResourceController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour
+{
+    public Text ResourceDescription;
+    public Text ResourceUpgradeCost;
+    public Text ResourceUnlockCost;
+
+    private ResourceConfig _config;
+    private int _level = 1;
+
+    public void SetConfig(ResourceConfig config)
+    {
+        _config = config;
+        ResourceDescription.text = $"{_config.Name} Lv. {_level}\n+{GetOutput().ToString("0")}";
+        ResourceUnlockCost.text = $"Unlock Cost\n{_config.UnlockCost}";
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{GetUpgradeCost()}";
+    }
+
+    public double GetOutput()
+    {
+        return _config.Output * _level;
+    }
+
+    public double GetUpgradeCost()
+    {
+        return _config.UpgradeCost * _level;
+    }
+
+    public double GetUnlockCost()
+    {
+        return _config.UnlockCost;
+    }
+
+}

12d34e7101569fdf4f60234df48612a97523a7f6
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-09-17 13:25:20 +0700

    fix tap, add game build

--- Incremental-Game/Assets/Scripts/TapArea.cs
+++ Incremental-Game/Assets/Scripts/TapArea.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TapArea : MonoBehaviour
+public class TapArea : MonoBehaviour, IPointerDownHandler
 {
     public void OnPointerDown(PointerEventData eventData)
     {
